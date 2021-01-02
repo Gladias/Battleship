2875e3d6ffed53e82d0c0ba8e6ea86c2db95676f
Gladias <alek.drozdz@gmail.com>
2021-01-02 23:22:37 +0100

    Add generation of two boards

--- board.py
+++ board.py
@@ -1,3 +1,19 @@
+from cell import Cell
+import const
+
 class Board:
-    def __init__(self):
-        self.
+    def __init__(self, board_number, is_visible):
+        is_visible = is_visible
+        self.board = []
+        for row in range(10):
+            line = []
+            for column in range(10):
+                if board_number == 1:
+                    element = Cell((const.FIRST_CELL_POSITION[0] + (column * (const.CELL_SIZE + const.CELL_GAP)),
+                                   const.FIRST_CELL_POSITION[1] + (row * (const.CELL_SIZE + const.CELL_GAP))))
+                else:
+                    element = Cell((const.SECOND_CELL_POSITION[0] + (column * const.CELL_SIZE),
+                                    const.SECOND_CELL_POSITION[1] + (row * const.CELL_SIZE)))
+
+                line.append(element)
+            self.board.append(line)
